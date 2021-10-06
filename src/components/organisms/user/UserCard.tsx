b19eeed573bea2d74244e1904e57a6c4ce3fa2e7
haryu <hryau6@gmail.com>
2021-10-06 16:08:42 +0900

    add: UerCard organisms

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,23 @@
+import { memo, VFC } from "react";
+import { Box, Stack, WrapItem, Image, Text } from '@chakra-ui/react'
+
+type Props = {
+  userName: string
+  iamgeUrl: string
+  fullName: string
+}
+
+export const UserCard: VFC<Props> = memo((props) => {
+  const { userName, iamgeUrl, fullName } = props
+  return (
+    <WrapItem >
+      <Box w="260" h="260" bg="gray" borderRadius="10px" shadow="md" p={4} _hover={{ opacity: ".6", cursor: "pointer" }}>
+        <Stack textAlign="center">
+          <Image src={iamgeUrl} boxSize="160px" borderRadius="full" alt={userName} m="auto" />
+          <Text fontSize="lg" fontWeight="bold" color="white">{userName}</Text>
+          <Text fontSize="md">{fullName}</Text>
+        </Stack>
+      </Box>
+    </WrapItem>
+  )
+})
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,19 +1,12 @@
-import { Box, Stack, Wrap, WrapItem, Image, Text } from "@chakra-ui/react";
+import { Wrap } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <>
-      <Wrap p={{ base: 4, md: 10}}>
-        <WrapItem >
-          <Box w="260" h="260" bg="gray" borderRadius="10px" shadow="md" p={4} _hover={{ opacity: ".6", cursor: "pointer"}}>
-            <Stack textAlign="center">
-              <Image src="https://source.unsplash.com/random" boxSize="160px" borderRadius="full" alt="user plofile" m="auto" />
-              <Text fontSize="lg" fontWeight="bold" color="white">Haryu</Text>
-              <Text fontSize="md">haryu ishida</Text>
-            </Stack>
-          </Box>
-        </WrapItem>
+      <Wrap p={{ base: 4, md: 10 }}>
+        <UserCard userName="haryu" iamgeUrl="https://source.unsplash.com/random" fullName="haryu ishida" />
       </Wrap>
     </>
   )
